d05a24f792e218c85bb2ea9729eb387e480895a7
JulianCodina <depedrojulianismael@gmail.com>
2023-08-11 16:08:39 -0300

    tarea subida

---
+++ src/actividad1.java
@@ -0,0 +1,30 @@
+/*Deberás crear un proyecto en Java que resuelva el siguiente problema:
+Dado un conjunto de tres números ingresados por el usuario a través de la consola,
+el programa debe ordenarlos de mayor a menor y mostrar el resultado por pantalla.*/
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class actividad1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese 3 numeros: ↓");
+
+        System.out.print("[1]: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("[2]: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("[3]: ");
+        int num3 = scanner.nextInt();
+
+        int[] numeros = {num1, num2, num3};
+        Arrays.sort(numeros);
+
+        System.out.println("Números ordenados de mayor a menor:");
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
