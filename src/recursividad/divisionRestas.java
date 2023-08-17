8c2c199804bc2cc32a40e1bffbb172021186cb0f
JulianCodina <depedrojulianismael@gmail.com>
2023-08-17 16:28:22 -0300

    ejercicio restas completo

--- src/recursividad/divisionRestas.java
+++ src/recursividad/divisionRestas.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class divisionRestas {
 
     public static int recursivo(int ndo, int sor){
-        if(ndo<sor)return 0;
+        if(ndo<sor)return 0;                            //si el dividendo es menor al divisor el resultado es 0
 
-        return 1 + recursivo(ndo-sor, sor);
+        return 1 + recursivo(ndo-sor, sor);        //el resultado sera la suma de las repeticiones hasta que el dividendo sea menor al divisor
     }
+
     public static int iterativo(int ndo, int sor){
-
+        int result=0;
+        if(ndo<sor) return 0;                           //si el dividendo es menor al divisor el resultado es 0
+        for(int i=1;ndo>=sor;i++){
+            ndo-=sor;
+            result=i;                                   //el resultado sera la suma de las repeticiones hasta que el dividendo sea menor al divisor
+        }
+        return result;
     }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Ingresar el dividendo: ");
@@ -20,6 +26,6 @@ public class divisionRestas {
         System.out.print("Ingresar el divisor: ");
         int dvsor = input.nextInt();
         System.out.print("\nEl resultado con recursividad es: " + recursivo(dvdendo, dvsor));
-        System.out.print("\nEl resultado con iteración es: " + iterativo(dvdendo, dvsor));
+        System.out.println("\nEl resultado con iteración es: " + iterativo(dvdendo, dvsor));
     }
 }
